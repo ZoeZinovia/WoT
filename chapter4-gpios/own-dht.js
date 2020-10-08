2e0ac5d4531967ff06b2d71301634bbfd0880c80
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-10-08 10:52:46 +0100

    Temperature and humdity sensor code

---
+++ chapter4-gpios/own-dht.js
@@ -0,0 +1,17 @@
+var sensorLib = require("node-dht-sensor");
+
+sensorLib.initialize(11, 12);
+var interval = setInterval(function(){
+    read();
+}, 2000);
+
+function read(){
+    var readout = sensorLib.read();
+    console.log("Temperature: " + readout.temperature.toFixed(2) + "C, " + "humidity: " + readout.humidity.toFixed(2) + "%");
+};
+
+process.on("SIGINT", function(){
+    clearInterval(interval);
+    console.log("All finished and got nowhere to go :(");
+    process.exit();
+});
