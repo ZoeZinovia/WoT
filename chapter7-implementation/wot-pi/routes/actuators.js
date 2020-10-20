bf80757a616be982f698dab7fd3b30ac4926f92f
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-10-20 12:41:27 +0100

    debigging 3

--- chapter7-implementation/wot-pi/routes/actuators.js
+++ chapter7-implementation/wot-pi/routes/actuators.js
@@ -9,15 +9,15 @@ router.route("/").get(function(req, res, next){
 });
 
 router.route("/leds").get(function(req, res, next){
-    req.result = resources.pi.actuators.leds;
-    next();
-    // res.send(resources.pi.actuators.leds);
+    // req.result = resources.pi.actuators.leds;
+    // next();
+    res.send(resources.pi.actuators.leds);
 });
 
 router.route("/leds/:id").get(function(req, res, next){
-    req.result = resources.pi.actuators.leds[req.params.id];
-    next();
-    // res.send(resources.pi.actuators.leds[req.params.id]);
+    // req.result = resources.pi.actuators.leds[req.params.id];
+    // next();
+    res.send(resources.pi.actuators.leds[req.params.id]);
 }).put(function(req, res, next){
     var selectedLed = resources.pi.actuators.leds[req.params.id];
     selectedLed.value = req.body.value;
--- chapter7-implementation/wot-pi/routes/sensors.js
+++ chapter7-implementation/wot-pi/routes/sensors.js
@@ -3,27 +3,27 @@ var express = require("express"),
     resources = require("./../resources/model");
 
 router.route("/").get(function(req, res, next){
-    req.result = resources.pi.sensors;
-    next();
-    // res.send(resources.pi.sensors);
+    // req.result = resources.pi.sensors;
+    // next();
+    res.send(resources.pi.sensors);
 });
 
 router.route("/pir").get(function(req, res, next){
-    req.result = resources.pi.sensors.pir;
-    next();
-    // res.send(resources.pi.sensors.pir);
+    // req.result = resources.pi.sensors.pir;
+    // next();
+    res.send(resources.pi.sensors.pir);
 });
 
 router.route("/temperature").get(function(req, res, next){
-    req.result = resources.pi.sensors.temperature;
-    next();
-    // res.send(resources.pi.sensors.temperature);
+    // req.result = resources.pi.sensors.temperature;
+    // next();
+    res.send(resources.pi.sensors.temperature);
 });
 
 router.route("/humidity").get(function(req, res, next){
-    req.result = resources.pi.sensors.humidity;
-    next();
-    // res.send(resources.pi.sensors.humidity);
+    // req.result = resources.pi.sensors.humidity;
+    // next();
+    res.send(resources.pi.sensors.humidity);
 });
 
 module.exports = router;
