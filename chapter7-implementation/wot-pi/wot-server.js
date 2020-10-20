659706b5478f0229e6283ab5ecb69a130c5355f8
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-10-20 09:36:06 +0100

    updating wot-server.js file

--- chapter7-implementation/wot-pi/wot-server.js
+++ chapter7-implementation/wot-pi/wot-server.js
@@ -1,13 +1,13 @@
 var httpServer = require("./servers/http"),
     resources = require("./resources/model");
 
-var ledPlugin = require("./plugins/internal/pirPlugin"),
+var ledPlugin = require("./plugins/internal/ledPlugin"),
     pirPlugin = require("./plugins/internal/pirPlugin"),
     dhtPlugin = require("./plugins/internal/dhtPlugin");
 
 ledPlugin.start({"simulate": false, "frequency" : 5000});
 pirPlugin.start({"simulate": false, "frequency" : 5000});
-dhtPlugin.start({"simulate": false, "frequency" : 10000});
+dhtPlugin.start({"simulate": false, "frequency" : 5000});
 
 var server = httpServer.listen(resources.pi.port, function(){
     console.info("The CEOT Pi is up and running on port %s", resources.pi.port);
