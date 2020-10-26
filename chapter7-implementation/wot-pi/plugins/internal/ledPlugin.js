fa690cee53000ccff0b364cdc7f8861b18ce958b
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-10-26 14:30:59 +0000

    led plugin debug 30

--- chapter7-implementation/wot-pi/plugins/internal/ledPlugin.js
+++ chapter7-implementation/wot-pi/plugins/internal/ledPlugin.js
@@ -1,34 +1,11 @@
 var resources = require("./../../resources/model"),
-    onChange = require("on-change"),
     interval,
-    // actuator,
-    // model,
     model = resources.pi.actuators,
     pluginName = model.leds[1].name + " & " + model.leds[2].name,
     localParams = {"simulate" : false, "frequency" : 10000};
 
-var oldValue = model.leds["2"].value;
-
-// import { Observable } from 'dist/object-observer.min.js';
-// const observableModel = Observable.from(model.leds["2"]);
-
-// var CorePlugin = require('./../corePlugin').CorePlugin,
-//     util = require('util'),
-//     utils = require('./../../utils/utils.js');
-
-// var LedsPlugin = exports.LedsPlugin = function (params) { //#A
-//     CorePlugin.call(this, params, 'leds',
-//         stop, simulate, ['ledState'], switchOnOff); //#B
-//     model = this.model;
-//     this.addValue(false);
-// };
-// util.inherits(LedsPlugin, CorePlugin); //#C
-
 exports.start = function(params) {
     localParams = params;
-    // setInterval(() => {
-    //     poll();
-    // }, 2000);
     if(localParams.simulate){
         simulate(); //assuming this is used if you don't have a physical Raspberry Pi
     } else {
@@ -60,12 +37,11 @@ exports.switchOnOff = function switchOnOff(){
     if(!localParams.simulate){
         var gpio = require("onoff").Gpio;
         var led2 = new gpio(model.leds["2"].gpio, "out");
-        var value2 = + (!oldValue);
+        var value2 = + (!model.leds["2"].value);
         console.log("write with value " + value2);
         led2.write(value2, function(){
             console.log("Changed LED2 state to: " + model.leds["2"].value);
         });
-        oldValue = !oldValue;
     }
 }
 
