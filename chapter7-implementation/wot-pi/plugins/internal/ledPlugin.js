f2b658521635e26d990588d251472dd748e383a0
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-10-26 14:34:15 +0000

    led plugin debug 21

--- chapter7-implementation/wot-pi/plugins/internal/ledPlugin.js
+++ chapter7-implementation/wot-pi/plugins/internal/ledPlugin.js
@@ -37,7 +37,7 @@ exports.switchOnOff = function switchOnOff(){
     if(!localParams.simulate){
         var gpio = require("onoff").Gpio;
         var led2 = new gpio(model.leds["2"].gpio, "out");
-        var value2 = + (!model.leds["2"].value);
+        var value2 = + (model.leds["2"].value);
         console.log("write with value " + value2);
         led2.write(value2, function(){
             console.log("Changed LED2 state to: " + model.leds["2"].value);
