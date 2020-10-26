067da1aa8fc27e25554a14fe9e095e3765736ada
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-10-26 14:23:05 +0000

    led plugin debugging 18

--- chapter7-implementation/wot-pi/resources/model.js
+++ chapter7-implementation/wot-pi/resources/model.js
@@ -4,4 +4,5 @@ var set = function(resource, value){
     resources.pi.actuators.leds["2"] = value;
     ledPlugin.switchOnOff();
 }
-module.exports = resources, set;
+module.exports = resources;
+module.exports = set;
