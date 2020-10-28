7f8dab2a526eb409d4cd5bd84ad3556610ba048e
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-10-28 11:48:04 +0000

    11

--- chapter7-implementation/wot-pi/servers/websockets.js
+++ chapter7-implementation/wot-pi/servers/websockets.js
@@ -2,7 +2,7 @@ var WebSocketServer = require('ws').Server,
     updates = [];
 
 var resources = require("./../resources/model").resources;
-var thisObject = require("./../resources/example").exampleObject;
+var thisObject = require("./../resources/example").resources;
 
 exports.listen = function(server) {
   var wss = new WebSocketServer({server: server}); //#A
