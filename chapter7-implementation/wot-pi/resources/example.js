268cdfb641268f4aac86b84689cd9589465a7ecd
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-10-28 11:46:17 +0000

    8

--- chapter7-implementation/wot-pi/resources/example.js
+++ chapter7-implementation/wot-pi/resources/example.js
@@ -1,3 +1,15 @@
-var exampleObject = require("./resources.json");
+var exampleObject = require('./resources.json');
+var notifyChange = require("./../servers/websockets").notifyChange;
+
+var set = function(thing, val){
+    thing.value  = val;
+    notifyChange(thing);
+    console.log("Change made to: " + thing.name + ", new value: " + thing.value);
+}
+var get= function(thing){
+    return thing;
+}
 
 module.exports.exampleObject = exampleObject;
+module.exports.set = set;
+module.exports.get = get;
--- chapter7-implementation/wot-pi/servers/websockets.js
+++ chapter7-implementation/wot-pi/servers/websockets.js
@@ -2,7 +2,7 @@ var WebSocketServer = require('ws').Server,
     updates = [];
 
 var resources = require("./../resources/model").resources;
-var thisObject = require("./../resources/model").resources;
+var thisObject = require("./../resources/example").exampleObject;
 
 exports.listen = function(server) {
   var wss = new WebSocketServer({server: server}); //#A
