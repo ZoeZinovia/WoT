c29ff74fffbdb7578f7dae8d1e2f0a452caf6bd7
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-11-05 11:39:21 +0000

    changing led2 page back to put

--- chapter7-implementation/wot-pi/routes/actuators.js
+++ chapter7-implementation/wot-pi/routes/actuators.js
@@ -20,7 +20,7 @@ router.route("/leds/:id").get(function(req, res, next){
     req.result = resources.pi.actuators.leds[req.params.id];
     next();
     // res.send(resources.pi.actuators.leds[req.params.id]);
-}).post(function(req, res, next){
+}).put(function(req, res, next){
     console.log("POST request made");
     var selectedLed = resources.pi.actuators.leds[req.params.id];
     console.log(req.body);
