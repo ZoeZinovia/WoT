06870b5e0e4770fa61ea7939791234e7146e2bf3
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-11-05 11:53:09 +0000

    creating web pages to access pi

--- chapter7-implementation/wot-pi/middleware/converter.js
+++ chapter7-implementation/wot-pi/middleware/converter.js
@@ -6,15 +6,15 @@ module.exports = function(){
     return function(req, res, next){
         console.info("Representation converter middleware has been called!!");
         if(req.result){
-            if(req.accepts("json")){
-                console.info("JSON representation selected.");
-                res.send(req.result);
-                return;
-            }
             if(req.accepts("html")){
                 console.info("HTML representation selected.");
                 var transform = {'tag': 'div', 'html': '${name} : ${value}'};
                 res.send(json2html.transform(req.result, transform));
+                return;
+            }
+            if(req.accepts("json")){
+                console.info("JSON representation selected.");
+                res.send(req.result);
                 return;
             }
             if(req.accepts("application/x-msgpack")){
