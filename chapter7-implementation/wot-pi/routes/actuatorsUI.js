4f8c1636e8ff84fdac035310ad0d1466af1c3ca6
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-11-05 12:27:08 +0000

    2

--- chapter7-implementation/wot-pi/routes/actuatorsUI.js
+++ chapter7-implementation/wot-pi/routes/actuatorsUI.js
@@ -1,9 +1,9 @@
-var express = require("express"),
-    router = express.Router(),
-    resources = require("../resources/model").resources,
-    setResource = require("../resources/model").set,
-    ledPlugin = require("./../plugins/internal/ledPlugin");
+// var express = require("express"),
+//     router = express.Router(),
+//     resources = require("../resources/model").resources,
+//     setResource = require("../resources/model").set,
+//     ledPlugin = require("./../plugins/internal/ledPlugin");
 
-    app.get("/UI/pi/actuators/leds/:id", function(req, res){
-        res.sendFile(path.join(__dirname+"./../public/led" + req.params.id + "page.html"));
-    });
+//     app.get("/UI/pi/actuators/leds/:id", function(req, res){
+//         res.sendFile(path.join(__dirname+"./../public/led" + req.params.id + "page.html"));
+//     });
