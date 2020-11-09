4b0e2c1a850f6abc8a9e170acb8f0410345244b6
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2020-11-09 12:24:45 +0000

    testing with mosquito

--- chapter7-implementation/wot-pi-led/simple-LED.js
+++ chapter7-implementation/wot-pi-led/simple-LED.js
@@ -9,69 +9,74 @@ var thngApiKey=config.thngApiKey;
 var status=false;
 var updateInterval;
 
-var client = mqtt.connect("mqtts://mqtt.evrythng.com:8883", { 
-    username: 'authorization',
-    password: thngApiKey
-});
+// var client = mqtt.connect("mqtts://mqtt.evrythng.com:8883", { 
+//     username: 'authorization',
+//     password: thngApiKey
+// });
 
-ledPlugin.start({"simulate": false, "frequency" : 5000});
+var client  = mqtt.connect("mqtt://test.mosquitto.org",{clientId:"mqttjs01"});
+client.on("connect",function(){	
+    console.log("connected");
+})
 
-client.on("connect", function(){
-    console.log("connected :)");
-    client.subscribe(thngUrl + "/properties/");
-    client.subscribe(thngUrl+'/actions/all');
-    // if (! updateInterval) updateInterval = setInterval(updateProperties, 5000);
-});
+// ledPlugin.start({"simulate": false, "frequency" : 5000});
 
-client.on("message", function(topic, message){
-    var resources = topic.split("/");
-    console.log("Resources: " + resources);
-    if (resources[1] && resources[1] === "thngs"){
-        if(resources[2] && resources[2] === thngId){
-            if(resources[3] && resources[3] === "properties"){
-                var property = JSON.parse(message);
-                console.log("Property was updated: " + property[0].key + " = " + property[0].value);
-            } else if (resources[3] && resources[3] === "actions"){
-                var action = JSON.parse(message);
-                handleAction(action);
-            }
-        }
-    }
-});
+// client.on("connect", function(){
+//     console.log("connected :)");
+//     client.subscribe(thngUrl + "/properties/");
+//     client.subscribe(thngUrl+'/actions/all');
+//     // if (! updateInterval) updateInterval = setInterval(updateProperties, 5000);
+// });
 
-function handleAction(action){
-    switch(action.type){
-        case "_setStatus":
-            console.log("ACTION: _setStatus changed to: " + action.customFields.status);
-            status = Boolean(action.customFields.status);
-            updateProperty("status", status);
-            ledPlugin.switchOnOff();
-            break;
-        default:
-            console.log("ACTION: Unknown action type: " + action.type);
-            break;
-    }
-}
+// client.on("message", function(topic, message){
+//     var resources = topic.split("/");
+//     console.log("Resources: " + resources);
+//     if (resources[1] && resources[1] === "thngs"){
+//         if(resources[2] && resources[2] === thngId){
+//             if(resources[3] && resources[3] === "properties"){
+//                 var property = JSON.parse(message);
+//                 console.log("Property was updated: " + property[0].key + " = " + property[0].value);
+//             } else if (resources[3] && resources[3] === "actions"){
+//                 var action = JSON.parse(message);
+//                 handleAction(action);
+//             }
+//         }
+//     }
+// });
 
-function updateProperties(){
-    var voltage = (219.5 + Math.random()).toFixed(3);
-    updateProperty("voltage", voltage);
+// function handleAction(action){
+//     switch(action.type){
+//         case "_setStatus":
+//             console.log("ACTION: _setStatus changed to: " + action.customFields.status);
+//             status = Boolean(action.customFields.status);
+//             updateProperty("status", status);
+//             ledPlugin.switchOnOff();
+//             break;
+//         default:
+//             console.log("ACTION: Unknown action type: " + action.type);
+//             break;
+//     }
+// }
 
-    var current = (Math.random()*10).toFixed(3);
-    updateProperty("current", current);
+// function updateProperties(){
+//     var voltage = (219.5 + Math.random()).toFixed(3);
+//     updateProperty("voltage", voltage);
 
-    var power = (voltage*current*(0.6+Math.random()/10)).toFixed(3);
-    updateProperty("power", power);
-};
+//     var current = (Math.random()*10).toFixed(3);
+//     updateProperty("current", current);
 
-function updateProperty(property, value){
-    client.publish(thngUrl+"/properties/" + property, '[{"value": '+ value + '}]');
-}
+//     var power = (voltage*current*(0.6+Math.random()/10)).toFixed(3);
+//     updateProperty("power", power);
+// };
 
-process.on("SIGINT", function(){
-    clearInterval(updateInterval);
-    console.log("EXITING!");
-    updateProperty("livenow", false);
-    client.end();
-    process.exit();
-});
+// function updateProperty(property, value){
+//     client.publish(thngUrl+"/properties/" + property, '[{"value": '+ value + '}]');
+// }
+
+// process.on("SIGINT", function(){
+//     clearInterval(updateInterval);
+//     console.log("EXITING!");
+//     updateProperty("livenow", false);
+//     client.end();
+//     process.exit();
+// });
